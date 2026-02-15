86375eacc1ed31441c81318e127ba7cd4f3f1345
coderbyahmed <uzairazizkhan405@gmail.com>
2026-02-15 02:35:45 -0800

    PC SE IMPROVE KIYA

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,4 @@
+// mobile slide menu bar 
 
   var burger = document.getElementById("burger");
   var mobileMenu = document.getElementById("mobileMenu");
@@ -13,3 +14,44 @@
     document.body.style.overflow = "auto";
   });
 
+
+// counter efect dala hai 
+
+  var counters = document.querySelectorAll(".counter");
+
+var startCounter = (counter) => {
+  var target = +counter.getAttribute("data-target");
+  var count = 0;
+
+  var increment = target / 200; 
+
+  var updateCounter = () => {
+    count += increment;
+
+    if (count < target) {
+      counter.innerText = Math.ceil(count);
+    } else {
+      counter.innerText = target;
+      clearInterval(interval);
+    }
+  };
+
+  var interval = setInterval(updateCounter, 15);
+};
+
+
+var observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      startCounter(entry.target);
+      observer.unobserve(entry.target); 
+    }
+  });
+}, {
+  threshold: 0.5
+});
+
+
+counters.forEach(counter => {
+  observer.observe(counter);
+});
